58d9edb8dea6d53e77b1f14a6893c7065204c41b
ZhangMin1998 <1464253423@qq.com>
2023-10-07 00:30:43 +0800

    使用AntD重构MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,26 @@
 import React, { FC } from 'react'
-// import styles from './MainLayout.module.scss'
+import styles from './MainLayout.module.scss'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+
+const { Header, Footer, Content } = Layout
 
 const MainLayout:FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>
+        问卷调查
+        &copy;
+        2023 - present. Created by zhangmin
+      </Footer>
+    </Layout>
   )
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+// import { Button } from 'antd'
 
 const Home: FC = () => {
   const navigate = useNavigate()
