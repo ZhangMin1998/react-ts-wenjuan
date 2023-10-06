6f5e884ebb56afc7d725998caca40a89cb329233
ZhangMin1998 <1464253423@qq.com>
2023-10-06 14:03:14 +0800

    添加css

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,15 +7,38 @@ type PropsType = {
   isPublished: boolean,
   isStar: boolean,
   answerCount: number,
-  cteateAt: string
+  createdAt: string
 }
 
 const Card: FC<PropsType> = (props: PropsType) => {
-  const { _id } = props
+  const { _id, title, createdAt, answerCount, isPublished } = props
   return (
-   <>
-    <p>Card {_id}</p>
-   </>
+   <div className={styles.container}>
+    <div className={styles.title}>
+      <div className={styles.left}>
+        <a href="#">{title}</a>
+      </div>
+      <div className={styles.right}>
+        {isPublished ? <span style={{color: 'green'}}>已发布</span> : <span>未发布</span>}
+        &nbsp;
+        <span>答卷: {answerCount}</span>
+        &nbsp;
+        <span>{createdAt}</span>
+      </div>
+    </div>
+
+    <div className={styles['button-container']}>
+      <div className={styles.left}>
+        <button>编辑问卷</button>
+        <button>数据统计</button>
+      </div>
+      <div className={styles.right}>
+        <button>标星</button>
+        <button>复制</button>
+        <button>删除</button>
+      </div>
+    </div>
+   </div>
   )
 }
 
--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -4,10 +4,10 @@ import Card from '../../components/Card/Card'
 
 const List: FC = () => {
   const [list, setList] = useState([
-    { _id: 'q1', title: '问题1', isPublished: false, isStar: false, answerCount: 5, cteateAt: '3月10日 13:24' },
-    { _id: 'q2', title: '问题2', isPublished: true, isStar: false, answerCount: 5, cteateAt: '3月10日 13:24' },
-    { _id: 'q3', title: '问题3', isPublished: false, isStar: false, answerCount: 5, cteateAt: '3月10日 13:24' },
-    { _id: 'q4', title: '问题4', isPublished: true, isStar: false, answerCount: 5, cteateAt: '3月10日 13:24' }
+    { _id: 'q1', title: '问题1', isPublished: false, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
+    { _id: 'q2', title: '问题2', isPublished: true, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
+    { _id: 'q3', title: '问题3', isPublished: false, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
+    { _id: 'q4', title: '问题4', isPublished: true, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' }
   ])
   return (
    <>
