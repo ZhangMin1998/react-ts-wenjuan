dc4b68bbaf6dbc97132e0b718c00ca88c44f1f86
zhangmin <zhangm@suntang.com>
2023-10-07 15:59:06 +0800

    使用AntD重构ManageLayout

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,3 @@ function App() {
 }
 
 export default App
-
-// import React from 'react'
-// import './App.css'
-
-// // import List from './pages/List/List'
-// import { RouterProvider } from 'react-router-dom'
-// import routerConfig from './router/index'
-
-// function App() {
-//   return <RouterProvider router={routerConfig}></RouterProvider>
-// }
-
-// export default App
--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,23 +1,52 @@
 import React, { FC } from 'react'
 import styles from './ManageLayout.module.scss'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+import { Button, Space, Divider } from 'antd'
+import { BarsOutlined, DeleteOutlined, PlusOutlined, StarOutlined } from '@ant-design/icons'
 
 const ManageLayout:FC = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  console.log('pathname', pathname)
+  
+
   return (
     <>
-      <div className={styles.container}>ManageLayout left</div>
-      <div className={styles.left}>
-        <p>left</p>
-        <button>创建问卷</button>
-        <br/>
-        <a>我的问卷</a>
-        <br/>
-        <a>星标问卷</a>
-        <br/>
-        <a>回收站</a>
-      </div>
-      <div className={styles.right}>
-        <Outlet />
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <Space direction='vertical'>
+            <Button type="primary" size='large' icon={<PlusOutlined />}>创建问卷</Button>
+            <Divider style={{borderTop: 'transparent'}} />
+
+            <Button
+              type={pathname.startsWith('/manage/list') ? 'default' : 'text'}
+              size='large'
+              icon={<BarsOutlined />}
+              onClick={() => navigate('/manage/list')}
+            >
+              我的问卷
+            </Button>
+            <Button
+              type={pathname.startsWith('/manage/star') ? 'default' : 'text'}
+              size='large'
+              icon={<StarOutlined />}
+              onClick={() => navigate('/manage/star')}
+            >
+              星标问卷
+            </Button>
+            <Button
+              type={pathname.startsWith('/manage/trash') ? 'default' : 'text'}
+              size='large'
+              icon={<DeleteOutlined />}
+              onClick={() => navigate('/manage/trash')}
+            >
+              回收站
+            </Button>
+          </Space>
+        </div>
+        <div className={styles.right}>
+          <Outlet />
+        </div>
       </div>
     </>
   )
