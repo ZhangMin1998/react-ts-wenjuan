545cafca6a3f6dc9dc63fbf15ac7e2e93658f8d5
zhangmin <zhangm@suntang.com>
2023-10-07 16:52:22 +0800

    使用AntD开发首页

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,23 @@
 import React, { FC } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
-// import { Button } from 'antd'
+import styles from './Home.module.scss'
+import { Button, Typography } from 'antd';
+
+const { Title, Paragraph } = Typography;
 
 const Home: FC = () => {
   const navigate = useNavigate()
 
-  const clickLogin = () => {
-    navigate('/login')
-  }
-
   return (
-    <div>
-      <p>home</p>
-      <div>
-        <button onClick={clickLogin}>登录</button>
-        <Link to='/register?a=10'>注册</Link>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>
+          已累计创建问卷 100 份，发布问卷 90 份， 收到答卷 980 份。
+        </Paragraph>
+        <div>
+          <Button type='primary' onClick={() => navigate('/manage/list')}>开始使用</Button>
+        </div>
       </div>
     </div>
   )
