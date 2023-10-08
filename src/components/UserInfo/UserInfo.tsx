c906abb0cfe23a504157ee678fd56dad2b23a54d
ZhangMin1998 <1464253423@qq.com>
2023-10-08 22:16:24 +0800

    头像更改

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { Avatar } from 'antd'
 
 const UserInfo:FC = () => {
-  const url = 'http://git.suntang.com/uploads/-/system/user/avatar/105/avatar.png?width=400'
+  const url = '	https://lf3-cdn2-tos.bytegoofy.com/ppx/mp/static/media/squared.9ff960bc.svg'
   return (
     <Link to='/login'>
       <Avatar src={<img src={url} alt="avatar" />} />
