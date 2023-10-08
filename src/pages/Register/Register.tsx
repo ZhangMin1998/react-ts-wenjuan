3e28493f907d411502f3c0dad58d18045e7a6989
zhangmin <zhangm@suntang.com>
2023-10-08 17:35:40 +0800

    使用AntD的Form组件开发注册页

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,9 +1,85 @@
 import React, { FC } from 'react'
-// import styles from './Register.module.scss'
+import styles from './Register.module.scss'
+import { Typography, Space, Button, Checkbox, Form, Input } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+const { Title } = Typography
 
 const Register:FC = () => {
+  type FieldType = {
+    username?: string;
+    password?: string;
+    confirm?: string;
+    remember?: string;
+  }
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  }
+
   return (
-    <p>Register</p>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}><UserAddOutlined /> </Title>
+          <Title level={2}>注册</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="用户名"
+            name="username"
+            rules={[{ required: true, message: '请输入用户名!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="确认密码"
+            name="confirm"
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item<FieldType>
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to='/login'>已有账号，去登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
 
