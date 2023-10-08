86f0e26423353c7b39fa23a8054abf8b6e48d415
zhangmin <zhangm@suntang.com>
2023-10-08 16:09:12 +0800

    Avatar组件更改头像

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react'
 import styles from './UserInfo.module.scss'
 import { Link } from 'react-router-dom' 
+import { Avatar } from 'antd'
 
 const UserInfo:FC = () => {
+  const url = 'http://git.suntang.com/uploads/-/system/user/avatar/105/avatar.png?width=400'
   return (
-    <Link to='/login'>登录</Link>
+    <Link to='/login'>
+      <Avatar src={<img src={url} alt="avatar" />} />
+    </Link>
   )
 }
 
