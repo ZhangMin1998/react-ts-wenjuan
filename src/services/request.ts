3c76d5171cbd07343737338faf3dd91d6f20727f
zhangmin <zhangm@suntang.com>
2023-10-09 18:29:36 +0800

    配置axios的response拦截器-统一处理错误提示

---
+++ src/services/request.ts
@@ -0,0 +1,8 @@
+import axios from './ajax'
+import type { ResDataType } from './ajax'
+
+export async function getQuestionService(id: string): Promise<ResDataType> {
+  const url = `/api/question/${id}`
+  const data = (await axios.get(url)) as ResDataType
+  return data
+}
