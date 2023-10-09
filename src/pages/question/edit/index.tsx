1d21b86a14992a012e4e6da6f68183cf35eab4dc
ZhangMin1998 <1464253423@qq.com>
2023-10-10 00:13:12 +0800

    编辑页修改，添加loading等

--- src/pages/question/edit/index.tsx
+++ src/pages/question/edit/index.tsx
@@ -1,21 +1,26 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getQuestionService } from '../../../services/request'
 
 const Edit:FC = () => {
   const { id = '' } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [questionData, setQuestionData] = useState({})
 
   useEffect(() => {         // 异步函数无法直接在useEffect使用
     async function fn() {
       const data = await getQuestionService(id)
       console.log('data', data)
+      setQuestionData(data)
+      setLoading(false)
     }
     fn()
   }, [id])
 
-  return (
-    <p>Edit {id}</p>
-  )
+  return <div>
+    <p>编辑页</p>
+    { loading ? <p>loading</p> : <p>{JSON.stringify(questionData)}</p> }
+  </div>
 }
 
 export default Edit
