c438c191658d55d17092b97b6d0ee9c5ed738f2d
ZhangMin1998 <1464253423@qq.com>
2023-10-10 23:18:57 +0800

    使用useRequest获取问卷列表

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import styles from './common.module.scss'
-import { Typography } from 'antd'
+import { Typography, Spin  } from 'antd'
 import Card from '../../components/Card/Card'
 import ListSearch from '../../components/ListSearch/ListSearch'
 // import { useSearchParams } from 'react-router-dom'
+import { getQuestionListService } from '../../services/request'
+import { useRequest } from 'ahooks'
 
 const { Title } = Typography
 
@@ -12,12 +14,28 @@ const List: FC = () => {
   // console.log('aaa', searchParams.get('aaa'))
   
 
-  const [list, setList] = useState([
-    { _id: 'q1', title: '问题1', isPublished: false, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
-    { _id: 'q2', title: '问题2', isPublished: true, isStar: true, answerCount: 5, createdAt: '3月10日 13:24' },
-    { _id: 'q3', title: '问题3', isPublished: false, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
-    { _id: 'q4', title: '问题4', isPublished: true, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' }
-  ])
+  // const [list, setList] = useState([
+  //   // { _id: 'q1', title: '问题1', isPublished: false, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
+  //   // { _id: 'q2', title: '问题2', isPublished: true, isStar: true, answerCount: 5, createdAt: '3月10日 13:24' },
+  //   // { _id: 'q3', title: '问题3', isPublished: false, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' },
+  //   // { _id: 'q4', title: '问题4', isPublished: true, isStar: false, answerCount: 5, createdAt: '3月10日 13:24' }
+  // ])
+  // const [total, setTotal] = useState(0)
+
+  // useEffect(() => {         // 异步函数无法直接在useEffect使用
+  //   async function load() {
+  //     const data = await getQuestionListService()
+  //     console.log('data66', data)
+  //     setList(data.list)
+  //     setTotal(data.total)
+  //   }
+  //   load()
+  // }, [])
+
+  // 使用useRequest替换上面的
+  const { data = {}, loading } = useRequest(getQuestionListService)
+  const { list = [], total = 0 } = data
+
   return (
    <>
     <div className={styles.header}>
@@ -30,9 +48,14 @@ const List: FC = () => {
     </div>
 
     <div className={styles.content}>
+      {loading && (
+        <div style={{textAlign: 'center'}}>
+          <Spin size="large" />
+        </div>
+      )}
       {/* 问卷列表 */}
       {
-        list.length && list.map(item => {
+        (!loading && list.length > 0) && list.map((item: any) => {
           const { _id } = item
           return <Card key={_id} {...item} />
         })
--- src/services/request.ts
+++ src/services/request.ts
@@ -14,3 +14,10 @@ export async function createQuestionService(): Promise<ResDataType> {
   const data = (await axios.post(url)) as ResDataType
   return data
 }
+
+// 获取（查询）问卷列表
+export async function getQuestionListService(): Promise<ResDataType> {
+  const url = `/api/question`
+  const data = (await axios.get(url)) as ResDataType
+  return data
+}
