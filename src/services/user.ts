3083c59fcd623e90b8777b11f3f9a2a4d7360c82
ZhangMin1998 <1464253423@qq.com>
2023-10-12 23:18:52 +0800

    登录、注册、用户信息接口

---
+++ src/services/user.ts
@@ -0,0 +1,31 @@
+import axios, { ResDataType } from './ajax'
+
+
+// 获取用户信息
+export async function getUserInfoService(): Promise<ResDataType> {
+  const url = `/api/user/info`
+  const data = (await axios.get(url)) as ResDataType
+  return data
+}
+
+// 注册
+export async function registerService(
+  username: string,
+  password: string
+): Promise<ResDataType> {
+  const url = `/api/user/register`
+  const body = { username, password }
+  const data = (await axios.post(url, body)) as ResDataType
+  return data
+}
+
+// 注册
+export async function loginService(
+  username: string,
+  password: string
+): Promise<ResDataType> {
+  const url = `/api/user/login`
+  const body = { username, password }
+  const data = (await axios.post(url, body)) as ResDataType
+  return data
+}
