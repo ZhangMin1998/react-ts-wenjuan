62e31f41a7d403a1463d2032cc2de2dec75eff3c
ZhangMin1998 <1464253423@qq.com>
2023-10-13 00:55:15 +0800

    获取用户信息、退出

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,14 +1,52 @@
 import React, { FC } from 'react'
 import styles from './UserInfo.module.scss'
-import { Link } from 'react-router-dom' 
-import { Avatar } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import { Link, useNavigate } from 'react-router-dom' 
+import { Avatar, Button, message } from 'antd'
+import { getUserInfoService } from '../../services/user'
+import { useRequest } from 'ahooks'
+import { removeToken } from '../../utils/user-token'
+
+const url = '	https://lf3-cdn2-tos.bytegoofy.com/ppx/mp/static/media/squared.9ff960bc.svg'
 
 const UserInfo:FC = () => {
-  const url = '	https://lf3-cdn2-tos.bytegoofy.com/ppx/mp/static/media/squared.9ff960bc.svg'
+  const navigate = useNavigate()
+
+  const { data } = useRequest(getUserInfoService)
+  const { username } = data || {}
+
+  const logout = () => {
+    removeToken()
+    message.success('退出成功')
+    navigate(`/login`)
+  }
+
+  const UserInfo = (
+    <>
+      <div className={styles.infoBox}>
+        <Avatar src={<img src={url} alt="avatar" />} />
+        <div>
+          <Button type='link' onClick={logout}>{username}</Button>
+        </div>
+      </div>
+    </>
+  )
+
+  const Login = (
+    <>
+      <div>
+        <UserOutlined /> <span style={{color: '#fff'}}>去登录</span>
+      </div>
+    </>
+  )
+  
   return (
-    <Link to='/login'>
-      <Avatar src={<img src={url} alt="avatar" />} />
-    </Link>
+    <div>
+      { username ? UserInfo : Login }
+    </div>
+    // <Link to='/login'>
+    //   <Avatar src={<img src={url} alt="avatar" />} />
+    // </Link>
   )
 }
 
