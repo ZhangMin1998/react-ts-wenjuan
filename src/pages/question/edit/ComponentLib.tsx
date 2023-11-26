56d23bcf8a2a38310dbda12f73a8ac07d2382beb
ZhangMin1998 <1464253423@qq.com>
2023-11-27 00:30:04 +0800

    把组件分组显示在组件库中

--- src/pages/question/edit/ComponentLib.tsx
+++ src/pages/question/edit/ComponentLib.tsx
@@ -1,17 +1,30 @@
 import React, { FC } from 'react'
 import { Typography } from 'antd'
-import { componentConfGroup } from '../../../components/QuestionComponents'
+import { componentConfGroup, ComponentConfType } from '../../../components/QuestionComponents'
+import styles from './ComponentLib.module.scss'
 
 const { Title } = Typography
+
+function getComponent(c: ComponentConfType) {
+  const { title, type, Component} = c
+  return <div className={styles.wrapper}>
+    <div className={styles.component}>
+      <Component />
+    </div>
+  </div>
+}
 
 const ComponentLib:FC = () => {
   return <>
     {componentConfGroup.map((group, index) => {
-      const { groupId, groupName } = group
+      const { groupId, groupName, components } = group
       return <div key={groupId}>
         <Title level={3} style={{fontSize: '16px', marginTop: index > 0 ? '20px' : 0}}>
           {groupName}
         </Title>
+        <div>
+          {components.map(item => getComponent(item))}
+        </div>
       </div>
     })}
   </>
