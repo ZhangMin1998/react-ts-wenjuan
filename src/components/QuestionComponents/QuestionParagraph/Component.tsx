b8b33942a0b6569e530218b79f5a28294fb5e0db
ZhangMin1998 <1464253423@qq.com>
2023-11-29 21:37:05 +0800

    为Paragraph组件增加换行功能-充分考虑安全性

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -8,16 +8,20 @@ const QuestionParagraph: FC<QuestionParagraphPropsType> =  (props:QuestionParagr
 
   const { text = '', isCenter = false } = { ...QuestionParagraphDefaultProps, ...props }
 
-  const t = text.replaceAll('\n', '<br>')
+  // const t = text.replaceAll('\n', '<br>')
+  const textList = text.split('\n')
 
-  function getHtml() {
-    return { __html: t}
-  }
   
   return (
     <div>
       <Paragraph style={{textAlign: isCenter ? 'center' : 'start', marginBottom: '0'}}>
-        <span dangerouslySetInnerHTML={getHtml()}></span>
+        {/* <span dangerouslySetInnerHTML={{ __html: t}}></span> 尽量不要使用*/}
+        {textList.map((t, index) => (
+          <span key={index}>
+            {index > 0 && <br />}
+            {t}
+          </span>
+        ))}
       </Paragraph>
     </div>
   )
