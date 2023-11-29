498df4d83cd86e86c5f509a367c40edd5eab3ad4
ZhangMin1998 <1464253423@qq.com>
2023-11-29 21:31:06 +0800

    dangerouslySetInnerHTML

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -7,11 +7,17 @@ const { Paragraph } = Typography
 const QuestionParagraph: FC<QuestionParagraphPropsType> =  (props:QuestionParagraphPropsType) => {
 
   const { text = '', isCenter = false } = { ...QuestionParagraphDefaultProps, ...props }
+
+  const t = text.replaceAll('\n', '<br>')
+
+  function getHtml() {
+    return { __html: t}
+  }
   
   return (
     <div>
       <Paragraph style={{textAlign: isCenter ? 'center' : 'start', marginBottom: '0'}}>
-        {text}
+        <span dangerouslySetInnerHTML={getHtml()}></span>
       </Paragraph>
     </div>
   )
