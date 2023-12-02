d64855ea71c4b48c45b1e1c2bad24a48973ababb
ZhangMin1998 <1464253423@qq.com>
2023-12-02 15:36:34 +0800

    开发QuestionRadio属性表单-可动态添加删除选项

--- src/components/QuestionComponents/QuestionRadio/PropComponent.tsx
+++ src/components/QuestionComponents/QuestionRadio/PropComponent.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect } from 'react'
-import { Form, Input, Checkbox, Select } from 'antd'
+import { Form, Input, Checkbox, Select, Button, Space } from 'antd'
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons'
 import { QuestionRadioPropsType } from './interface'
 
 const PropComponent:FC<QuestionRadioPropsType> = (props: QuestionRadioPropsType) => {
@@ -25,6 +26,39 @@ const PropComponent:FC<QuestionRadioPropsType> = (props: QuestionRadioPropsType)
   >
     <Form.Item label="标题" name='title' rules={[{required: true, message: '请输入标题'}]}>
       <Input />
+    </Form.Item>
+    <Form.Item label="选项">
+      <Form.List name='options'>
+        {(fields, { add, remove }) => (
+          <>
+            {/* 遍历所有的选项，可删除 */}
+            {fields.map(({key, name}, index) => {
+              
+              return <Space key={key} align='baseline'>
+                {/* 输入框 */}
+                <Form.Item
+                  name={[name, 'text']}
+                  rules={[
+                    {
+                      required: true,
+                      // whitespace: true,
+                      message: "请输入选项内容",
+                    },
+                  ]}
+                >
+                  <Input placeholder='输入选项文字...' />
+                </Form.Item>
+                {/* 删除按钮 */}
+                { index > 1 && <MinusCircleOutlined onClick={() => remove(name)} /> }
+              </Space>
+            })}
+            {/* 添加选项 */}
+            <Form.Item>
+              <Button type='link' onClick={() => add({text: '', value: ''})} icon={<PlusOutlined/>} block>添加选项</Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
     </Form.Item>
     <Form.Item label="默认选中" name='value'>
       <Select value={value} options={options.map(({text, value}) => ({
